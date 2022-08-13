836ef394b7c08405a8ed54ad96866ca23d5e5c5c
peyman <khezli1999@gmail.com>
2022-08-13 19:52:14 +0430

    Mini Project with length properti.

--- is-balance.js
+++ is-balance.js
@@ -1,36 +1,23 @@
 
 
-let text = 'String Methods and Properties Primitive values, like "John Doe", cannot have properties or methods (because they are not objects). But with JavaScript, methods and properties are also available to primitive values, because JavaScript treats primitive values as objects when executing methods and properties.';
 
 
-let len = text.length < 100 ? 'The String is Short' : 'The String is Long';
 
-console.log(len);
+function MyFunction() {
 
-/* 
+    let mytext1;
+    mytext1 = document.getElementById('mytext').value;
+    let result1;
+    result1 = document.getElementById('result');
 
-There are 3 methods for extracting a part of a string:
+    let condition = mytext1.length < 10 ? result1.innerHTML = mytext1.length : result1.innerHTML = 'The String is Long';
 
-slice(start, end)
+}
 
-substring(start, end)
 
-substr(start, length)
 
-*/
 
-let parts = text.slice(-20 , -1);
 
-let parts1 = text.slice(1 , 20);
 
-let parts2 = text.slice(8);
-
-let include = parts.includes('and') ? '0' : '1';
-
-console.log(include);
-
-console.log(parts2);
-
-console.log(text.substring(13));
 
 
