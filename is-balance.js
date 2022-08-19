24983bdee93b8e2a15850218c5f4482e1e6d1053
peyman <khezli1999@gmail.com>
2022-08-19 20:44:12 +0430

    Modify Project

--- is-balance.js
+++ is-balance.js
@@ -3,18 +3,27 @@ function MyFunction() {
 
     let mytext1;
     mytext1 = document.getElementById('mytext').value;
-    let result1;
-    result1 = document.getElementById('result');
+    let result;
+    result = document.getElementById('result');
+    let input = document.getElementById('Pey');
 
 
-    if (mytext1.length < 10) {
+    if (mytext1.length < 20) {
 
+        result.innerHTML = mytext1.length;
 
     } else {
-        'DDS'
+
+        result.innerHTML = 'This Text is long';
+        input.innerHTML = document.getElementById('mytext').value;
+        document.getElementById('mytext').value = '';
+
     }
 
 }
+
+
+
 
 
 
